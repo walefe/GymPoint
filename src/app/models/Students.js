2105bd69951711bd45e4c5887a8ab782bd676cff
Walefe <walefedev@gmail.com>
2019-11-05 12:18:44 -0300

    add model students.

---
+++ src/app/models/Students.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Students extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        age: Sequelize.STRING,
+        weight: Sequelize.STRING,
+        height: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'instructor_id' });
+  }
+}
+
+export default Students;
