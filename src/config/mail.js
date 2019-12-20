60698f00ef65f53d7cf1e6888606bf7ffa288ab9
Walefe <walefedev@gmail.com>
2019-12-20 14:10:01 -0300

    chore: add config mail.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6816fecf3471c2',
+    pass: '7e612734713fdd',
+  },
+  default: {
+    from: 'Equipe GymPoint <noreply@gympoint.com>',
+  },
+};
