211adf1025bcd6cf55c0df5a3221de3b934f25b4
Walefe <walefedev@gmail.com>
2019-12-26 20:25:23 -0300

    Chore: create model help orders.

---
+++ src/app/models/HelpOrders.js
@@ -0,0 +1,24 @@
+import Sequelize, { Model } from 'sequelize';
+
+class HelpOrders extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        question: Sequelize.STRING,
+        answer: Sequelize.STRING,
+        answer_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Students, { foreignKey: 'student_id' });
+  }
+}
+
+export default HelpOrders;
