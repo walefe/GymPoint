cfc880d3f5fd6693ebbb79dafcea87ae8ff15c9f
Walefe <walefedev@gmail.com>
2019-12-26 20:26:39 -0300

    Chore: add routes for help orders and student help orders;

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import StudentController from './app/controllers/StudentController';
 import PlansController from './app/controllers/PlansController';
 import RegistrationController from './app/controllers/RegistrationController';
 import CheckinsController from './app/controllers/CheckinsController';
+import HelpOrdersController from './app/controllers/HelpOrdersController';
+import StudentHelpOrdersController from './app/controllers/StudentHelpOrdersController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -14,6 +16,9 @@ routes.post('/sessions', SessionController.store);
 
 routes.get('/students/:id/checkins', CheckinsController.index);
 routes.post('/students/:id/checkins', CheckinsController.store);
+
+routes.get('/students/:id/help-orders', StudentHelpOrdersController.index);
+routes.post('/students/:id/help-orders', StudentHelpOrdersController.store);
 
 routes.post('/users', UserController.store);
 
@@ -31,5 +36,8 @@ routes.get('/registration', RegistrationController.index);
 routes.post('/registration', RegistrationController.store);
 routes.put('/registration/:id', RegistrationController.update);
 routes.delete('/registration/:id', RegistrationController.delete);
+
+routes.get('/help-orders', HelpOrdersController.index);
+routes.put('/help-orders/:id/answer', HelpOrdersController.update);
 
 export default routes;
